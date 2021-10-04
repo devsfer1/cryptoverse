92191b9ef686b082cc93e4cfce3e9a79b6050769
Fernando Chaves <fernando.chaves16@hotmail.com>
2021-10-04 06:48:14 -0300

    feat: added components

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
-import { default as Navbar } from './navbar';
+export { default as Navbar } from './Navbar.jsx';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as Exchanges } from './Exchanges';
+export { default as Homepage } from './Homepage';
+export { default as News } from './News';
